257df2d0ea33a738ff241c1a02ce056c5aae22fb
planetofthecode <rajkumar.karnakar@outlook.com>
2024-06-28 00:30:15 +0530

    split screen code

---
+++ split-screen-app/src/App.js
@@ -0,0 +1,18 @@
+import { SplitScreen } from "./SliptScreen";
+const LeftHandComponent = () => {
+	return <h1 style={{ backgroundColor: 'green' }}>Left!</h1>;
+}
+
+const RightHandComponent = () => {
+	return <p style={{ backgroundColor: 'red' }}>Right!</p>;
+}
+
+function App() {
+	return (
+		<SplitScreen
+			left={LeftHandComponent}
+			right={RightHandComponent} />
+	);
+}
+
+export default App;
